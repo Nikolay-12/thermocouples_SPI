b9562ddd623f0538f4cdbf1e0fd4a19e108c9eb4
Nikolay-12 <104552191+Nikolay-12@users.noreply.github.com>
2025-09-18 16:28:32 +0300

    Update thermocouples.cpp
    code template

--- src/thermocouples.cpp
+++ src/thermocouples.cpp
@@ -1,1 +1,30 @@
 // thermocouples reading
+#include "thermocouples.h"
+
+namespace thermocouples
+{
+  void init() 
+  {
+    
+  }
+
+  void read_all_temp_once() 
+  {
+    
+  }
+
+  void read_all_temp() 
+  {
+    
+  }
+
+  void read_one_temp_point() 
+  {
+    
+  }
+
+  void stop_reading_temp()
+  {
+    
+  }  
+}
