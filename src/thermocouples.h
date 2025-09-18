c5c2e1eb0e42ea957f21f6c897611075a3f8bfdb
Nikolay-12 <104552191+Nikolay-12@users.noreply.github.com>
2025-09-18 15:53:12 +0300

    Update thermocouples.h
    add "read_all_temp_once" method

--- src/thermocouples.h
+++ src/thermocouples.h
@@ -1,21 +1,16 @@
-#include <SPI.h>
+#pragma once
 
 #define Reg_num 2 // кол-во регистров
 #define Reg_pins Reg_num * 8 // Общее кол-во пинов на всех регистрах
 boolean Reg_state_outPins[Reg_pins]; // массив состояний пинов
-
-
-#define NANO_CLK_PIN 13 // Пин SCK термопар
-#define NANO_MISO_PIN 12 // ПИН MISO термопар
-#define HC595_DS_PIN 2 // Пин данных для сдвиговых регистов ("путешествующий ноль")
-#define HC595_STcp_PIN 3 // Пин для защелкивания выходов сдвиговых регистов
-#define HC595_SHcp_PIN 4 // Пин тактовых импульсов для сдвиговых регистов
 
 #define THERMOCOUPLES_NUM 8
 
 namespace thermocouples
 {
     void init();
+    void read_all_temp_once();
     void read_all_temp();
+    void read_one_temp_point();
 }
 
