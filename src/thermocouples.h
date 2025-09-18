48c445eded594de10525e498be201aa3f199921d
Nikolay-12 <104552191+Nikolay-12@users.noreply.github.com>
2025-09-19 02:39:58 +0300

    Update thermocouples.h
    add "read_temp_from_one_thermocouple_once" and "read_temp_from_one_thermocouple" methods

--- src/thermocouples.h
+++ src/thermocouples.h
@@ -7,7 +7,8 @@ namespace thermocouples
     void init();
     void read_all_temp_once();
     void read_all_temp();
-    void read_one_temp_point();
+    void read_temp_from_one_thermocouple_once(thermocouple_index);
+    void read_temp_from_one_thermocouple(thermocouple_index);
     void stop_reading_temp();
     float get_temp_celsius(thermocouple_index);
 }
