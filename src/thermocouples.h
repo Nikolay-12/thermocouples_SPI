8eb6b4cf6a94c01240f583c9ce871991b0d365da
Nikolay-12 <104552191+Nikolay-12@users.noreply.github.com>
2025-09-21 17:25:54 +0300

    Update thermocouples.h
    add data type to thermocouples indexes

--- src/thermocouples.h
+++ src/thermocouples.h
@@ -7,9 +7,9 @@ namespace thermocouples
     void init();
     void read_all_temp_once();
     void read_all_temp();
-    void read_temp_from_one_thermocouple_once(thermocouple_index);
-    void read_temp_from_one_thermocouple(thermocouple_index);
+    void read_temp_from_one_thermocouple_once(uint8_t thermocouple_index);
+    void read_temp_from_one_thermocouple(uint8_t thermocouple_index);
     void stop_reading_temp();
-    double get_temp_celsius(thermocouple_index);
+    double get_temp_celsius(uint8_t thermocouple_index);
 }
 
